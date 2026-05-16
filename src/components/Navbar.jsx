deb8183f211cb7fd14646805b1f654ed181a89c2
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-05-16 12:10:48 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom'; 
 import { LayoutDashboard, CheckSquare, Wallet, BookOpen, BarChart3, Home, Menu, X, ChevronRight } from 'lucide-react'; 
 import '../styles/Navbar.css'; 
-import LogoImg from '../assets/logo.PG'; 
+import LogoImg from '../assets/logo.jPG'; 
 
 const Navbar = () => { 
   const [isMenuOpen, setIsMenuOpen] = useState(false); 
